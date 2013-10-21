d178e8ce03e3b2b285006bc0c1ecc767e7fa18e8
Marcin Musial <marcin.musial@gmail.com>
2013-10-21 03:12:41 +0200

    add tree to linux instalation script

--- linux/prepare-ubuntu.sh
+++ linux/prepare-ubuntu.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java7-installer oracle-jdk7-installer
-sudo apt-get -y install unity-tweak-tool build-essential clang-3.4 clang-3.4-doc cmake mercurial vim ruby2.0 ruby2.0-doc libx11-dev libgl1-mesa-dev libsqlite3-dev sqlite3
+sudo apt-get -y install unity-tweak-tool build-essential clang-3.4 clang-3.4-doc cmake mercurial vim ruby2.0 ruby2.0-doc libx11-dev libgl1-mesa-dev libsqlite3-dev sqlite3 tree
 
 # android studio support
 sudo apt-get -y install lib32ncurses5
